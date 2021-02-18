40c0d3fa0990164e1dea1cb05313c9c6db15a404
Masato Ikeda <masato.ikeda@gmail.com>
2021-02-18 15:48:43 +0900

    Extract parameters to Env object

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {Runner} from './runner'
 
 async function run(): Promise<void> {
   try {
     const api_token = core.getInput('api-token')
-    const runner = new Runner(api_token)
+
+    const payload = github.context.payload
+    if (!payload.repository) {
+      throw new Error("payload doesn't include `repository` key")
+    }
+
+    const check_suite_id = payload.workflow_run.check_suite_id
+    const head_branch = payload.workflow_run.head_branch
+    const head_commit = payload.workflow_run.head_commit.id
+    const workflow = payload.workflow.name
+    const owner = payload.repository.owner.login
+    const repo = payload.repository.name
+
+    const runner = new Runner({
+      api_token,
+      check_suite_id,
+      head_branch,
+      head_commit,
+      workflow,
+      owner,
+      repo
+    })
     await runner.run()
   } catch (error) {
     core.setFailed(error.message)
